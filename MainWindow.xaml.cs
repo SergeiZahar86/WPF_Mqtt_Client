fbd2bd354310ffca57c6b932cc88fa34f25c5390
Сергей Захаров <sergei86zahar@mail.ru>
2020-11-26 15:36:05 +0700

    1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -40,21 +40,22 @@ namespace WPF_Mqtt_Client
 
             client.Connect("sergei", "admin", "admin"); // подключение к серверу ИндасХолдинг
         }
-        // этот код запускается при закрытии главного окна (конец приложения)
-        protected override void OnClosed(EventArgs e)
+        protected override void OnClosed(EventArgs e) // этот код запускается при закрытии главного окна (конец приложения)
         {
             client.Disconnect();
             base.OnClosed(e);
             App.Current.Shutdown();
         }
-        // этот код запускается при нажатии кнопки «Подписаться»
-        private void btnSubscribe_Click(object sender, RoutedEventArgs e)
+        private void btnSubscribe_Click(object sender, RoutedEventArgs e) // этот код запускается при нажатии кнопки «Подписаться»
         {
+            string Topic = "hello";
+            client.Subscribe(new string[] { Topic }, new byte[] { 2 });   
+            txtReceived.Text = "";
+            /*
             if (txtTopicSubscribe.Text != "")
             {
                 // вся тема
                 string Topic = "/sergei/" + txtTopicSubscribe.Text + "/test";
-
                 // подписаться на тему с QoS 2
                 client.Subscribe(new string[] { Topic }, new byte[] { 2 });   // нам нужны массивы
                 // в качестве параметров, потому что мы можем подписаться на разные темы одним вызовом
@@ -64,15 +65,18 @@ namespace WPF_Mqtt_Client
             {
                 System.Windows.MessageBox.Show("Вы должны ввести тему, чтобы подписаться!");
             }
+            */
         }
-        // этот код запускается при нажатии кнопки "Опубликовать"
-        private void btnPublish_Click(object sender, RoutedEventArgs e)
+        private void btnPublish_Click(object sender, RoutedEventArgs e) // этот код запускается при нажатии кнопки "Опубликовать"
         {
+            string Topic = "hello";
+            client.Publish(Topic, Encoding.UTF8.GetBytes(txtPublish.Text), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+
+            /*
             if (txtTopicPublish.Text != "")
             {
                 // вся тема
                 string Topic = "/sergei/" + txtTopicPublish.Text + "/test";
-
                 // опубликовать сообщение с QoS 2
                 client.Publish(Topic, Encoding.UTF8.GetBytes(txtPublish.Text), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
             }
@@ -80,20 +84,14 @@ namespace WPF_Mqtt_Client
             {
                 System.Windows.MessageBox.Show("Вы должны ввести тему для публикации!");
             }
+            */
         }
-        // этот код запускается при получении сообщения
-        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) // этот код запускается при получении сообщения
         {
             string ReceivedMessage = Encoding.UTF8.GetString(e.Message);
-            Dispatcher.Invoke(delegate {              // нам нужна эта конструкция, потому что код приема
-                //в библиотеке и пользовательский интерфейс с текстовым полем выполняются в разных потоках
-                txtReceived.Text = ReceivedMessage;
-            });
+            Dispatcher.Invoke(delegate {txtReceived.Text = ReceivedMessage;}); // нам нужна эта конструкция, потому что код приема
+                                                                               //в библиотеке и пользовательский интерфейс с текстовым полем выполняются в разных потоках
         }
-
-
-
-
     }
 }
     
